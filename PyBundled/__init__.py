b39de2ab500957867143311059f071ba1f48e2e6
AnasAito <khdimaty@gmail.com>
2022-03-19 14:49:22 +0100

    first commit

---
+++ PyBundled/__init__.py
@@ -0,0 +1,7 @@
+
+   
+print("my_package is being imported")  # noqa E402
+
+from .utils import my_function
+# we will be able to do: from my_module import my_function, or
+# my_module.my_function
---
+++ setup.py
@@ -0,0 +1,5 @@
+
+from setuptools import setup, find_packages
+
+setup(name="PyBundled",
+      packages=find_packages())
